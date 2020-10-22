676390f6bea5918f3adc9117c701687d1d858604
khizerkamal <khizerkamal.03@gmail.com>
2020-10-22 10:03:42 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import { Accordion } from './Components/Accordion'
+import { Dropdown } from './Components/Dropdown'
+import { Search } from './Components/Search'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const items = [
+    {
+        title: 'What is React?',
+        content: 'React is a Frontend library of Javascript.'
+    },
+    {
+        title: 'Why React is so popular?',
+        content: 'React is famous among frontend engineers.'
+    },
+    {
+        title: 'How to use React?',
+        content: 'React is used by making simple reusable components.'
+    }
+]
+
+const options = [
+    {
+        label: 'The Color Red',
+        value: 'red'
+    },
+    {
+        label: 'The Color Green',
+        value: 'green'
+    },
+    {
+        label: 'A Shade of Blue',
+        value: 'blue'
+    }
+]
+
+const App = () => {
+    const [selected, setSelected] = useState(options[0]);
+    const [showDropdown, setShowDropdown] = useState(true);
+
+    return (
+        <div>
+            <button onClick = {() => { setShowDropdown(!showDropdown) } } >Toggle Dropdown</button>
+            { showDropdown ?
+                <Dropdown 
+                selected = { selected }
+                onSelectedChange = { setSelected }
+                Options = { options } 
+             /> : null
+            }
+            
+        </div>
+    )
 }
 
-export default App;
+export default App
---
+++ src/Components/Search.js
@@ -0,0 +1,71 @@
+import React, { useState, useEffect} from 'react';
+import axios from 'axios';
+
+export const Search = () => {
+    
+    const [term, setTerm] = useState('programming');
+    const [result, setResult] = useState([]);
+    console.log(result)
+
+    useEffect(() => {
+        const searchTerm = async () => {
+            const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+                params: {
+                    action: 'query',
+                    list: 'search',
+                    origin: '*',
+                    format: 'json',
+                    srsearch: term,
+                }
+            });
+            setResult(data.query.search)
+        }
+        if (term && !result.length){
+            searchTerm()
+        } else {
+            const timeoutId = setTimeout(() => {
+                if (term) {
+                    searchTerm();
+                }
+            }, 1000)
+            
+            return(() =>{
+                clearTimeout(timeoutId)
+            })
+        }
+        
+    }, [term])
+
+    const renderedResults = result.map((result) => {
+        return (
+            <div key = { result.pageid } className = "item">
+                <div className = "right floated content">
+                    <a 
+                        href = {`https://en.wikipedia.org?curid=${result.pageid}`}
+                        className = "ui button ">Go</a>
+                </div>
+                <div className = "content">
+                    <div className = "header">
+                        { result.title }
+                    </div>
+                    <span dangerouslySetInnerHTML = {{ __html: result.snippet }}></span>
+                </div>
+            </div>
+        );
+    })
+
+    return (
+        <div>
+            <div className = "ui form">
+                <div className = "field">
+                    <label>Enter Search Term</label>
+                    <input 
+                        value = { term }
+                        onChange = { (e) => setTerm(e.target.value) }
+                        className = "input" />
+                </div>
+            </div>
+            <div className = "ui celled list"> { renderedResults } </div>
+        </div>
+    )
+}
